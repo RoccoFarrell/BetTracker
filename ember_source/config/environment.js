53b4d211415598b95f8295b49084dbd8247df91e
rocco farrell <rocco farrell>
2017-01-28 19:44:16 -0700

    figured out side nav with ember-paper

--- ember_source/config/environment.js
+++ ember_source/config/environment.js
@@ -6,7 +6,13 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     contentSecurityPolicy: {
-     'connect-src' : "'self' http://localhost:80"
+     'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline'",
+      'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com",
+      'font-src': "'self' fonts.gstatic.com",
+      'connect-src': "'self'",
+      'img-src': "'self' data:",
+      'media-src': "'self'"
       },
     locationType: 'auto',
     EmberENV: {
